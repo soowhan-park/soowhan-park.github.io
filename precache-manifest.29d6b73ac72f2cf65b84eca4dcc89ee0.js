8b78dd42355749a9e1243b66a24a8b05ad37e814
Soowhan Park <sp6682@nyu.edu>
2023-09-11 14:28:38 -0400

    Updates

---
+++ precache-manifest.29d6b73ac72f2cf65b84eca4dcc89ee0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191b835e5a44244b7093dc92f480688f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f911d75efdcae5174e4d",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "60de6540677d60bdf96c",
+    "url": "/static/css/main.da9bddc6.chunk.css"
+  },
+  {
+    "revision": "f911d75efdcae5174e4d",
+    "url": "/static/js/2.1edf9e24.chunk.js"
+  },
+  {
+    "revision": "b8a26cc07166dc30cf9055bb104c91cd",
+    "url": "/static/js/2.1edf9e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60de6540677d60bdf96c",
+    "url": "/static/js/main.7afb7848.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
