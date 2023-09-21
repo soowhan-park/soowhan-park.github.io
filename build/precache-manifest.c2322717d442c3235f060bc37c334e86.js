6b30fea124ca38ffbbb58edab9d3699c084925de
Soowhan Park <sp6682@nyu.edu>
2023-09-21 18:16:51 -0400

    fix typo

---
+++ build/precache-manifest.c2322717d442c3235f060bc37c334e86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2cb51622d425bb3d02332bffe430078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f911d75efdcae5174e4d",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "447e42e7a2d489109dea",
+    "url": "/static/css/main.da9bddc6.chunk.css"
+  },
+  {
+    "revision": "f911d75efdcae5174e4d",
+    "url": "/static/js/2.1edf9e24.chunk.js"
+  },
+  {
+    "revision": "b8a26cc07166dc30cf9055bb104c91cd",
+    "url": "/static/js/2.1edf9e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447e42e7a2d489109dea",
+    "url": "/static/js/main.8c2ea585.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
